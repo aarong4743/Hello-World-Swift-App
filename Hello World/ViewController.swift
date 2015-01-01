2f33d6ce5456575961e8a91bb343be62abf8628e
Aaron Graham <aarong4743@Aarons-MacBook-Pro-5.local>
2015-01-01 13:59:09 -0500

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Aaron Graham on 1/1/15.
+//  Copyright (c) 2015 Aaron Graham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
